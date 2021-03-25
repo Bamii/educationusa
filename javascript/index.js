a426d26aabb0270f0cb9c10090fd9e5a2bc22fad
Bami Ayo-Salami <bbamii@outlook.com>
2021-03-25 16:52:58 +0100

    updoot4 :)

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,13 @@
 (function(){
   const topBar = document.getElementById("top-strip-container");
   const bannerSection = document.getElementById("banner-section");
+  
+  const menuWrapper = document.getElementById("menu-wrapper");
+  const mobileMenu = document.getElementById("mobile-menu-button");
+  
   let isMenuOpen = false;
 
-  // sticky-header
+  /* sticky-header */
   document.onscroll = function() {
     const { y } = bannerSection.getBoundingClientRect();
     console.log("dssd")
@@ -12,5 +16,14 @@
     } else {
       !isMenuOpen && topBar.classList.add("transparent");
     }
+  }
+
+  /* menu SHIT! */
+  mobileMenu.onclick = function(e) {
+    openMobileMenu(e);
+  }
+
+  function openMobileMenu() {
+    menuWrapper.classList.toggle("visible");
   }
 })();
