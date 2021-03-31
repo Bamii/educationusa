2abca3200aa686f8fdb783a63f61b144f5c44e92
Bami Ayo-Salami <bbamii@outlook.com>
2021-03-31 16:53:16 +0100

    rushhhhhh

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,7 +2,12 @@
   const menuWrapper = document.getElementById("menu-wrapper");
   const mobileMenu = document.getElementById("mobile-menu-button");
 
-  const 
+  const loginNavButton = document.getElementById('login-nav-btn');
+  const loginModal = document.getElementById("login-modal")
+  const closeModal = document.getElementById('close-modal');
+  const signUpBtn = document.getElementById('signup-btn')
+  const loginBtn = document.getElementById('login-btn')
+
   /* menu SHIT! */
   mobileMenu.onclick = function(e) {
     openMobileMenu(e);
@@ -10,5 +15,21 @@
 
   function openMobileMenu() {
     menuWrapper.classList.toggle("visible");
+  }
+
+  loginNavButton.onclick = function() {
+    toggleModal()
+  }
+
+  closeModal.onclick = function() {
+    toggleModal()
+  }
+  
+  function toggleModal() {
+    loginModal.classList.toggle('visible');
+  }
+
+  signUpBtn.onclick = function() {
+    
   }
 })()
