ec9ecf1de64d3916f1498bc8790591c5a068d78c
Bami Ayo-Salami <bbamii@outlook.com>
2021-03-31 17:27:11 +0100

    rushhhhhh3

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,16 @@
 (function(){
   const topBar = document.getElementById("top-strip-container");
   const bannerSection = document.getElementById("banner-section");
+  const bannerControls = document.getElementsByClassName("slide-dot");
 
   const updateSectionPrev = document.getElementById('updates-slider-prev')
   const updateSectionNext = document.getElementById('updates-slider-next');
   const ITEM_RIGHT_MARGIN = 20;
+  const BANNER_ITEMS = 3;
+
+  let currentBannerIndex = 0;
+  let previousBannerIndex = 0;
+  
 
   /* sticky-header */
   document.onscroll = function() {
@@ -16,6 +22,29 @@
     }
   }
 
+  updateSectionNext.onclick = function() {
+    movePortfolio('right');
+  }
+
+  updateSectionPrev.onclick = function() {
+    movePortfolio('left');
+  }
+
+  /* banner related things */
+  Array.from(bannerControls).forEach(indicator => {
+    indicator.onclick = changeBannerIndicator;
+  });
+
+  setInterval(() => {
+    previousBannerIndex = currentBannerIndex;
+    currentBannerIndex = currentBannerIndex === BANNER_ITEMS-1 ? 0 : ++currentBannerIndex;
+    const currentTextContainer = document.querySelector(`[data-slide='${currentBannerIndex}'] .banner-slide-description-container`);
+
+    addAnimation(currentTextContainer);  
+    updateBanner(currentBannerIndex);
+  }, 8000);
+
+  /* functions */
   function movePortfolio(direction) {
     const updates = document.getElementById('updates-slider');
     const childrenLength = updates.children.length;
@@ -33,11 +62,23 @@
     updates.scrollLeft = index * (width + ITEM_RIGHT_MARGIN);
   }
 
-  updateSectionNext.onclick = function() {
-    movePortfolio('right');
+  function updateBanner(index) {
+    const bannerSlides = document.getElementById("banner-slides");
+    
+    bannerSlides.scrollLeft = index * window.innerWidth;
+    updateControls();
   }
 
-  updateSectionPrev.onclick = function() {
-    movePortfolio('left');
+  function updateControls() {
+    document.querySelector(`[data-slide-control-index="${previousBannerIndex}"]`).classList.remove('selected');
+    document.querySelector(`[data-slide-control-index="${currentBannerIndex}"]`).classList.add('selected');
+  }
+
+  function changeBannerIndicator(event) {
+    const element = event.target;
+    const position = element.dataset.slideControlIndex;
+    previousBannerIndex = currentBannerIndex;
+    currentBannerIndex = position;
+    updateBanner(currentBannerIndex);
   }
 })();
