b29ccf4b18d7634fb463f4b9978ad9a199265c8a
Bami Ayo-Salami <bbamii@outlook.com>
2021-04-01 23:55:09 +0100

    asfdad

--- javascript/beneficiaries.js
+++ javascript/beneficiaries.js
@@ -1,3 +1,15 @@
 (function() {
-  const beneficiary = document.querySelectorAll('')
+  const beneficiaries = document.querySelectorAll('[data-beneficiary]')
+  const maskClose = document.querySelector('[data-modal-close]')
+  const beneficiaryModal = document.getElementById('beneficiary-details-modal')
+
+  maskClose.onclick = function(el) {
+    beneficiaryModal.classList.remove('visible');
+  }
+
+  Array.from(beneficiaries).forEach(el => {
+    el.onclick = function() {
+      beneficiaryModal.classList.add('visible')
+    }
+  })
 })()
