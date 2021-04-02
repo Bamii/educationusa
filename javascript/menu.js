5b71e7fa269030563d0646c63d266a9890005f4a
Bami Ayo-Salami <bbamii@outlook.com>
2021-04-02 01:03:33 +0100

    update

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,13 +1,14 @@
 (function() {  
   const menuWrapper = document.getElementById("menu-wrapper");
-  const mobileMenu = document.getElementById("mobile-menu-button");
+  const mobileMenu = document.getElementById("mobile-menu-button")
+  const mobileMenuClose = document.getElementById('mobile-menu-close')
 
   const loginNavButton = document.getElementById('login-nav-btn');
   const mobileLoginNavButton = document.getElementById('mobile-login-nav-btn');
   const loginModal = document.getElementById("login-modal")
   const closeModals = document.querySelectorAll('[data-close-modal]');
   const signUpBtn = document.getElementById('signup-btn')
-  const loginBtn = document.getElementById('login-btn')
+  const loginBtn = document.getElementById('signin-btn')
 
   /* menu SHIT! */
   mobileMenu.onclick = function(e) {
@@ -26,6 +27,15 @@
   signUpBtn.onclick = function(el) {
     document.querySelector('[data-auth-target="signin"]').classList.remove('active')
     document.querySelector('[data-auth-target="signup"]').classList.add('active')
+  }
+
+  loginBtn.onclick = function() {
+    document.querySelector('[data-auth-target="signin"]').classList.add('active')
+    document.querySelector('[data-auth-target="signup"]').classList.remove('active')
+  }
+
+  mobileMenuClose.onclick = function() {
+    toggleMenuModal();
   }
 
   Array.from(closeModals).forEach(closeModal => {
